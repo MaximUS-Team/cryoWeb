7c219842e536f4eb93cfccad52ae49973247bc2a
Ashley Gillman <gillwoman@gmail.com>
2014-04-24 19:50:34 +1000

    added y axis, improved format

--- public/javascripts/currentTempPlot.js
+++ public/javascripts/currentTempPlot.js
@@ -1,11 +1,10 @@
 
 //var request = require('request');
 var data = [];// = [1,2,3];
-var margin = {top: 30, right: 30, bottom: 30, left: 50},
+var margin = {top: 30, right: 30, bottom: 30, left: 60},
     width = 500 - margin.left - margin.right,
     height = 200 - margin.top - margin.bottom;
 var datLength = 20;
-var barWidth = width/datLength;
 
 // initialise chart
 var chart = d3.select(".chart")
@@ -15,38 +14,45 @@ var chart = d3.select(".chart")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
 // create X axis
-var x = d3.scale.linear()
-    .domain([0, datLength])
-    .range([0, width]);
 var xAxis = d3.svg.axis()
-    .scale(x)
     .orient("bottom");
-chart.append("g")
+var xAxisG = chart.append("g")
     .attr("class","x axis")
     .attr("transform", "translate(0," + height + ")")
-    .call(xAxis);
 var yAxis = d3.svg.axis()
     .orient("left");
 var yAxisG = chart.append("g")
     .attr("class","y axis");
+yAxisG.append("text")
+    .attr("transform", "rotate(-90)")
+    .attr("y",-60)
+    .attr("x",-20)
+    .attr("dy", ".71em")
+    .style("text-anchor", "end")
+    .text("Temperature (K)");
 
 setInterval(function() {
     $.getJSON("./data?type=status&data=T", function(res) {
         data.push(Math.round(res.T*1000)/1000);
-        console.log(data);
         while (data.length > datLength) {
             data.shift();
         }
 
-        // create Y axis
+        // update axes
+        var x = d3.scale.linear()
+            .domain([0, data.length])
+            .range([0, width]);
+        var barWidth = width/data.length;
         var y = d3.scale.linear()
             .domain([1.1*d3.min(data)-0.1*d3.max(data), d3.max(data)])
             .range([height, 0]);
+        xAxisG.call(xAxis.scale(x));
         yAxisG.call(yAxis.scale(y));
 
         // data join
         var bar = chart.selectAll(".bar")
-            .data(data);
+            .data(data)
+            .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
         // update data
         bar.select("rect")
             .attr("y",function(d) { return y(d); })
