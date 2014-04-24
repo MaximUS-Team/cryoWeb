0bedb08a1bacaeeb8886024d6b001d8cabc7eff7
Ashley Gillman <gillwoman@gmail.com>
2014-04-24 17:49:06 +1000

    improved temp graph format

--- public/javascripts/currentTempPlot.js
+++ public/javascripts/currentTempPlot.js
@@ -1,82 +1,69 @@
 
 //var request = require('request');
 var data = [];// = [1,2,3];
-var margin = {top: 20, right: 30, bottom: 30, left: 40},
+var margin = {top: 30, right: 30, bottom: 30, left: 50},
     width = 500 - margin.left - margin.right,
-    height = 300 - margin.top - margin.bottom;
+    height = 200 - margin.top - margin.bottom;
 var datLength = 20;
 var barWidth = width/datLength;
 
 // initialise chart
 var chart = d3.select(".chart")
-    .attr("width", width)
-    .attr("height", height);
-
-var svg = d3.select("body").append("svg")
-    .attr("width", width)
-    .attr("height", 50)
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
   .append("g")
-    .attr("transform", "translate(32," + (25) + ")");
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// create X axis
+var x = d3.scale.linear()
+    .domain([0, datLength])
+    .range([0, width]);
+var xAxis = d3.svg.axis()
+    .scale(x)
+    .orient("bottom");
+chart.append("g")
+    .attr("class","x axis")
+    .attr("transform", "translate(0," + height + ")")
+    .call(xAxis);
+var yAxis = d3.svg.axis()
+    .orient("left");
+var yAxisG = chart.append("g")
+    .attr("class","y axis");
 
 setInterval(function() {
     $.getJSON("./data?type=status&data=T", function(res) {
         data.push(Math.round(res.T*1000)/1000);
+        console.log(data);
         while (data.length > datLength) {
             data.shift();
         }
 
-        // scale y
+        // create Y axis
         var y = d3.scale.linear()
             .domain([1.1*d3.min(data)-0.1*d3.max(data), d3.max(data)])
             .range([height, 0]);
+        yAxisG.call(yAxis.scale(y));
 
-        var bar = chart.selectAll("g")
+        // data join
+        var bar = chart.selectAll(".bar")
             .data(data);
+        // update data
         bar.select("rect")
             .attr("y",function(d) { return y(d); })
             .attr("height", function(d) { return height - y(d); })
             .attr("width", barWidth - 1);
-        bar.select("text")
-            .attr("x", barWidth - 3)
-            .attr("y", function(d) { return y(d) + 3; })
-            .attr("dy", ".75em")
-            .text(function(d) { return d; });
-            
+        // enter data
         var barEnter = bar.enter().append("g")
+            .attr("class","bar")
             .attr("transform", function(d, i) { return "translate(" + i * barWidth + ",0)"; });
         barEnter.append("rect")
             .attr("y",function(d) { return y(d); })
             .attr("height", function(d) { return height - y(d); })
             .attr("width", barWidth - 1);
-        barEnter.append("text")
-            .attr("x", barWidth - 3)
-            .attr("y", function(d) { return y(d) + 3; })
-            .attr("dy", ".75em")
-            .text(function(d) { return d; });
 
-        /*//Div Code
-        d3.select(".chart")
-          .selectAll("div")
-            .data(data)
-            .style("width", function(d) { return x(d) + "px"; })
-          .enter().append("div")
-            .style("width", function(d) { return x(d) + "px"; })
-            .text(function(d) { return d; });
-        */
-        // LOGGING
-        // Data Join
-        var text = svg.selectAll("text")
-            .data([data.length,data]);
-        // Update
-        text.attr("class","logdata");
-        // Enter
-        text.enter().append("text")
-            .attr("x", function(d, i) { return i * 20; })
-            .attr("dy", ".35em");
-        // Enter & Update
-        text.text(function(d) { return d; });
-        // Exit
-        text.exit().remove();
+        // AXIS
+        // join
+        chart.selectAll(".axis")
+        // update
     })
 },1000);
