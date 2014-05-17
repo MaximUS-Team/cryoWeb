6694447b91ae410749db27e28155b70e28553a80
Ashley Gillman <gillwoman@gmail.com>
2014-05-17 11:33:10 +1000

    file name

---
+++ routes/test-pc.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  //res.send('coolio'); // http status
+  query = req.query;
+  console.log(query);
+  if (query.address || query.port) {
+  	res.send(200);
+  } else {
+  	res.send(400);
+  }
+});
+router.put('/', function(req, res) {
+  //res.send('coolio'); // http status
+  query = req.query;
+  console.log(query);
+  res.send(200);
+});
+
+module.exports = router;
