05588d48a13cf03208bdaecc171419694f1d8cf1
Ashley Gillman <gillwoman@gmail.com>
2014-08-07 08:16:38 +1000

    unknown changes

--- test/fakeTestPC.py
+++ test/fakeTestPC.py
@@ -1,25 +1,27 @@
 import requests, random, time
 url = "http://jcu-cryo.herokuapp.com/test-pc"
-P = 0.01;
+P = 0.1;
 I = 0.005;
 D = 0.005;
 
-T = 293
+currTime = time.localtime();
+T = 295 + 4*(-(abs(11-(currTime.tm_hour + currTime.tm_min / 60.0 + currTime.tm_sec / 3600.0)%24)))/11
 goal = 293
-normalisation = 293;
 err = 0
 while True:
-	# goal signal, stochastic, but with 'normalisation' to bring it back
-	goal = goal + 0.1 * (random.random() - 0.5) + 0.05 * (normalisation - goal)
-
-	# PID for actual signal to smooth it out
+	currTime = time.localtime();
+	goal = 295 + 4*(-(abs(11-(currTime.tm_hour + currTime.tm_min / 60.0 + currTime.tm_sec / 3600.0)%24)))/11# normalise temp based on time
 	oldErr = err
 	err = goal - T
-	T = T + P * err + I * (err + oldErr) + D * (err - oldErr) + 0.002 * (random.random() - 0.5)
-
+	T = T + P * err + I * (err + oldErr) + D * (err - oldErr) + 0.004 * (random.random() - 0.5)
 	# Send data
 	payload = {'time': time.strftime('%Y/%m/%d %H:%M:%S'), 'T': '%.3f' % T}
 	r = requests.put(url, data=payload)
 	print(goal)
 	print(payload)
-	time.sleep(5)
+	time.sleep(1)
+	# sometimes change goal
+	#if random.random() < 0.1:
+	#	# goal signal, stochastic, but with 'normalisation' to bring it back
+	#	goal = goal + 0.1 * (random.random() - 0.5) + 0.05 * (normalisation - goal)
+	# PID for actual signal to smooth it out
