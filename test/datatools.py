46047432eb541f1f7923268a06ecc2791e080f7a
Ashley Gillman <gillwoman@gmail.com>
2014-08-07 15:37:26 +1000

    S param test data added, and sending from fake test PC. Ready for implementation Server side.

---
+++ test/datatools.py
@@ -0,0 +1,25 @@
+#23456789012345678901234567890123456789012345678901234567890123456789012
+import csv,time
+
+def parse_DAT_file(filename):
+	"""Tool for dealing with .dat files produced by Ashley Gillman's
+	LabVIEW program. Note: these will not be consistent with the .dat
+	files produced by Kenny Leong's LabVIEW programs.
+	"""
+	with open(filename, 'r') as f:
+		dateStr = f.readline()
+		date = time.strptime(dateStr,'%d/%m/%Y  %I:%M:%S %p\r\n')
+		tempStr = f.readline()
+		reader = csv.reader(f, delimiter='\t')
+		headers = next(reader, None)
+		reader = csv.DictReader(f, headers, delimiter='\t')
+		data = {'time': time.strftime(
+			'%Y/%m/%d %H:%M:%S',date),
+			'T': tempStr, 'Snp': []}
+		for line in reader:
+			data['Snp'].append(line)
+	return data
+
+if __name__ == '__main__':
+	data = parse_DAT_file('data/22K.DAT')
+	print data
--- test/fakeTestPC.py
+++ test/fakeTestPC.py
@@ -1,27 +1,39 @@
-import requests, random, time
+#23456789012345678901234567890123456789012345678901234567890123456789012
+import requests, random, time, os
+from datatools import parse_DAT_file
 url = "http://jcu-cryo.herokuapp.com/test-pc"
 P = 0.1;
 I = 0.005;
 D = 0.005;
 
+dataFiles = []
+for path, subdirs, files in os.walk(r'./data'):
+	for filename in files:
+		dataFiles.append(os.path.join(path, filename))
+
 currTime = time.localtime();
-T = 295 + 4*(-(abs(11-(currTime.tm_hour + currTime.tm_min / 60.0 + currTime.tm_sec / 3600.0)%24)))/11
+T = 295 + 4 * (-(abs(11 - (currTime.tm_hour + currTime.tm_min / 60.0 +
+	currTime.tm_sec / 3600.0) % 24))) / 11
 goal = 293
 err = 0
 while True:
+	# update time
 	currTime = time.localtime();
-	goal = 295 + 4*(-(abs(11-(currTime.tm_hour + currTime.tm_min / 60.0 + currTime.tm_sec / 3600.0)%24)))/11# normalise temp based on time
+
+	# update temp (T)
+	# normalise temp based on time (convincing on time of day vs temp)
+	goal = 295 + 4 * (-(abs(11 - (currTime.tm_hour + currTime.tm_min /
+		60.0 + currTime.tm_sec / 3600.0) % 24))) / 11
 	oldErr = err
 	err = goal - T
-	T = T + P * err + I * (err + oldErr) + D * (err - oldErr) + 0.004 * (random.random() - 0.5)
+	T = (T + P * err + I * (err + oldErr) + D * (err - oldErr) + 0.004 *
+		(random.random() - 0.5)) # PID
+
 	# Send data
-	payload = {'time': time.strftime('%Y/%m/%d %H:%M:%S'), 'T': '%.3f' % T}
+	payload = parse_DAT_file(random.choice(dataFiles))
+	payload['time'] = time.strftime('%Y/%m/%d %H:%M:%S')
+	payload['T'] = '%.3f' % T
 	r = requests.put(url, data=payload)
 	print(goal)
 	print(payload)
 	time.sleep(1)
-	# sometimes change goal
-	#if random.random() < 0.1:
-	#	# goal signal, stochastic, but with 'normalisation' to bring it back
-	#	goal = goal + 0.1 * (random.random() - 0.5) + 0.05 * (normalisation - goal)
-	# PID for actual signal to smooth it out
