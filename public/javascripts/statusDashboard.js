de037480271ce0973d8689c3f39308209f817e3a
Ashley Gillman <gillwoman@gmail.com>
2014-08-10 12:35:09 +1000

    TODO: move all graphing to a single js to make keeping track of graphs and variables more logical.

---
+++ public/javascripts/statusDashboard.js
@@ -0,0 +1,6 @@
+var margin = {top: 30, right: 30, bottom: 30, left: 60},
+	outer = {width: 500, height: 500},
+	inner = {
+		width: outer.width - margin.left - margin.right,
+		height: outer.height - margin.top - margin.bottom
+	};
