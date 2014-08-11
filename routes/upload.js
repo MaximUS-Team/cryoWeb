51a1942bc9323ad530358a75580c3957c16366d1
Ashley Gillman <gillwoman@gmail.com>
2014-08-12 09:04:11 +1000

    Fixed #1. Immediately save Snp before query can be overwritten.

--- routes/upload.js
+++ routes/upload.js
@@ -52,55 +52,57 @@ router.post('/', function(req, res) {
 
   // Update DB on S-params
   if (query.Snp) {
-    var currentSnpModel = mongoose.model('currentSnp');
+    (function() {
+      var Snp = query.Snp
+      var currentSnpModel = mongoose.model('currentSnp');
 
-    // only have one set of current S-params at a time, so first
-    // delete old data.
-    /*
-    currentSnpModel.find({}, function(err, docs) {
-      for (doc in docs) {
-        doc.remove();
-      }
-    });*/
-    currentSnpModel.remove({}, function (err) {
-      if (err) {
-        res.send(400);
-        return console.log("Error removing Snp from DB");
-      } else {
-        // check how many entries left
-        currentSnpModel.find({}, function(err, docs) {
-          // if empty
-          if (docs.length == 0) {
-            // add new S-params
-            if (query.Snp) {
-              for (var i=0; i < query.Snp.length; i++) {
-                var SnpPoint = query.Snp[i];
-                currentSnpModel.create({
-                  "Frequency": SnpPoint["Frequency"],
-                  "S11 Re": SnpPoint["S11 Re"],
-                  "S11 Im": SnpPoint["S11 Im"],
-                  "S12 Re": SnpPoint["S12 Re"],
-                  "S12 Im": SnpPoint["S12 Im"],
-                  "S21 Re": SnpPoint["S21 Re"],
-                  "S21 Im": SnpPoint["S21 Im"],
-                  "S22 Re": SnpPoint["S22 Re"],
-                  "S22 Im": SnpPoint["S22 Im"]
-                }, function(err, doc) {
-                  if (err) {
-                    res.send(400);
-                    return console.log("Error saving to DB");
-                  }
-                });
+      // only have one set of current S-params at a time, so first
+      // delete old data.
+      /*
+      currentSnpModel.find({}, function(err, docs) {
+        for (doc in docs) {
+          doc.remove();
+        }
+      });*/
+      currentSnpModel.remove({}, function (err) {
+        if (err) {
+          res.send(400);
+          return console.log("Error removing Snp from DB");
+        } else {
+          // check how many entries left
+          currentSnpModel.find({}, function(err, docs) {
+            // if empty
+            if (docs.length == 0) {
+              // add new S-params
+              if (Snp) {
+                for (var i=0; i < Snp.length; i++) {
+                  var SnpPoint = Snp[i];
+                  currentSnpModel.create({
+                    "Frequency": SnpPoint["Frequency"],
+                    "S11 Re": SnpPoint["S11 Re"],
+                    "S11 Im": SnpPoint["S11 Im"],
+                    "S12 Re": SnpPoint["S12 Re"],
+                    "S12 Im": SnpPoint["S12 Im"],
+                    "S21 Re": SnpPoint["S21 Re"],
+                    "S21 Im": SnpPoint["S21 Im"],
+                    "S22 Re": SnpPoint["S22 Re"],
+                    "S22 Im": SnpPoint["S22 Im"]
+                  }, function(err, doc) {
+                    if (err) {
+                      res.send(400);
+                      return console.log("Error saving to DB");
+                    }
+                  });
+                }
+              } else {
+                console.log(
+                  'for some reason query.Snp from upload is blank.')
               }
-            } else {
-              console.log(
-                'for some reason query.Snp from upload is blank.')
-              console.log(query.Snp)
             }
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+    })()
   }
 });
 
