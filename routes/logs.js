58290c7bb2b907390e9a12c44fb9a1e40da84bf8
Andrew Wickens <andrewjwickens@gmail.com>
2014-09-12 00:45:48 +1000

    Remove errors
    Comments out some functionality for time being.

--- routes/logs.js
+++ routes/logs.js
@@ -9,28 +9,27 @@ router.get('/', function(req, res) {
   });
 });
 
-
 module.exports = router;
 
-mongoose.connect('mongodb://admin:Cryogenic@oceanic.mongohq.com:10003/jcu-cryo', function(err){
-	if(!err){
-		console.log('connected to mongodb');
-	} else{
-		throw err;
-	}
-});
+// mongoose.connect('mongodb://admin:Cryogenic@oceanic.mongohq.com:10003/jcu-cryo', function(err){
+// 	if(!err){
+// 		console.log('connected to mongodb');
+// 	} else{
+// 		throw err;
+// 	}
+// });
 
-var schema = mongoose.Schema,
-ObjectID = Schema.ObjectID;
+// var schema = mongoose.Schema,
+// ObjectID = Schema.ObjectID;
 
-var Logs = new Schema({
-	log : string
-});
+// var Logs = new Schema({
+// 	log : string
+// });
 
-var Logs = mongoose.model('Logs', Logs);
+// var Logs = mongoose.model('Logs', Logs);
 
-app.get('/', function(req, res){
-	Logs.find({}, function(err, docs){
-		res.render('logs', {docs: docs});
-	});
-});
+// app.get('/', function(req, res){
+// 	Logs.find({}, function(err, docs){
+// 		res.render('logs', {docs: docs});
+// 	});
+// });
