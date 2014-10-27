a2f3891e901bad5e7e84c0099ad4fee17d345bd2
Andrew Wickens <andrewjwickens@gmail.com>
2014-10-27 11:23:14 +1000

    Rectify submit button not submitting

--- public/javascripts/submitTestSettings.js
+++ public/javascripts/submitTestSettings.js
@@ -1,8 +1,8 @@
 function updateTestSettings(){
 	//console.log("Update test settings!");
-	var sp = document.getElementsByName("rangeInput")[0].value;
-	var si = document.getElementsByName("rangeInput")[1].value;
-	var sd = document.getElementsByName("rangeInput")[2].value;
+	var sp = document.getElementsByName("rangeInputP")[0].value;
+	var si = document.getElementsByName("rangeInputI")[0].value;
+	var sd = document.getElementsByName("rangeInputD")[0].value;
 	var scm = document.getElementById("tempControl").value;
 	var spow = document.getElementById("power").value;
 	//console.log(sp + "," + si + "," + sd + "," + scm + "," + spow);
